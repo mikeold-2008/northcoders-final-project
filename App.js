328d8c57dc1cf8f509f067d7568fbef089ff8a9d
HannahLDugan <155827170+HannahLDugan@users.noreply.github.com>
2024-05-21 13:46:06 +0100

    added a login component for users

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { HomeScreen, RecentWorkout } from './src/HomeScreen';
+import LoginForm from './src/Login';
+
 
 const Stack = createStackNavigator();
 
@@ -19,6 +21,11 @@ export default function App() {
           name="RecentWorkout"
           component={RecentWorkout}
           options={{ title: 'Recent Workout' }}
+        />
+           <Stack.Screen
+          name="Login"
+          component={LoginForm}
+          options={{ title: 'LoginForm' }}
         />
       </Stack.Navigator>
     </NavigationContainer>
